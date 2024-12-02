aafb0959b95f53ff2b6537597ba4405378f77ef9
umashankar <umashankarp33@gmail.com>
2024-12-02 14:22:49 +0530

    minor change in frontend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance=axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://expense-tracker-3x4e.onrender.com/api',
 }); 
 
 export default instance;
