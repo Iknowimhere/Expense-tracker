fa61f35274ed5b98c0d5ad0ec1bed78632384392
umashankar <umashankarp33@gmail.com>
2024-12-02 14:13:55 +0530

    few changes in configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-import http from 'http';
-import app from './app.js';
-let PORT=5000;
+import app from "./app.js";
+import http from "http";
+
+let PORT=process.env.PORT || 5000;
 
 const server=http.createServer(app)
 
